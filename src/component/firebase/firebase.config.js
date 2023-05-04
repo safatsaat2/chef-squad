33d81957afb739af5e78c3f82ab74b52a37c39b9
safatsaat2 <sakibsafat47@gmail.com>
2023-05-04 19:53:22 +0600

    readme

--- src/component/firebase/firebase.config.js
+++ src/component/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9o3jD0tHlugBWsO9pU9dK7wENaldauvM",
-  authDomain: "chef-squad.firebaseapp.com",
-  projectId: "chef-squad",
-  storageBucket: "chef-squad.appspot.com",
-  messagingSenderId: "155813482336",
-  appId: "1:155813482336:web:ea3018c0d32f1585c39805"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
